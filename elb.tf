7c9ad33757b3f18a178dae98505a5b73ec1c4227
Hari krishna <139216697+9392162462@users.noreply.github.com>
2023-09-13 11:18:31 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "hari-terraform-elb"
   availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
 
   listener {
@@ -21,7 +21,7 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "hari-tf-elb"
   }
 }
 
@@ -30,16 +30,16 @@ resource "aws_elb" "bar" {
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.2"
   availability_zones      = ["ap-southeast-1a", "ap-southeast-1b"]
-  database_name           = "mydb"
-  master_username         = "raham"
-  master_password         = "Rahamshaik#444555"
+  database_name           = "hari"
+  master_username         = "hari"
+  master_password         = "harik#68"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
 }*/
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-0b825ad86ddcfb907"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
