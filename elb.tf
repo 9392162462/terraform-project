dae5111ca4e0eb1d6edcc91428bfe7421d000c8f
Hari krishna <139216697+9392162462@users.noreply.github.com>
2023-09-16 07:42:54 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "hari-terraform-elb"
-  availability_zones = ["ap-southeast-2a", "ap-southeast-2b"]
+  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
 
   listener {
     instance_port     = 80
@@ -39,7 +39,7 @@ resource "aws_elb" "bar" {
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-0dfb78957e4edea0c"
+  ami           = "ami-"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
